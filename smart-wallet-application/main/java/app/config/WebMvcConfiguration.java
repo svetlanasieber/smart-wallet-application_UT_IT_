a933c0de4bce67225f05dcf35bf6cccbfe64ea4b
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-18 23:37:52 +0100

    Update WebMvcConfiguration.java

--- smart-wallet-application/main/java/app/config/WebMvcConfiguration.java
+++ smart-wallet-application/main/java/app/config/WebMvcConfiguration.java
@@ -14,15 +14,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableMethodSecurity
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
-    // SecurityFilterChain - начин, по който Spring Security разбира как да се прилага за нашето приложение
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        // authorizeHttpRequests - конфиг. за група от ендпойнти
-        // requestMatchers - достъп до даден ендпойнт
-        // .permitAll() - всеки може да достъпи този ендпойнт
-        // .anyRequest() - всички заявки, които не съм изброил
-        // .authenticated() - за да имаш достъп, трябва да си аутентикиран
         http
                 .authorizeHttpRequests(matchers -> matchers
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
@@ -31,8 +25,6 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
-//                        .usernameParameter("username")
-//                        .passwordParameter("password")
                         .defaultSuccessUrl("/home", true)
                         .failureUrl("/login?error")
                         .permitAll())
